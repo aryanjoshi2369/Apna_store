782750e327160d58536aed9cc71df82f9f21e00b
Aryan <aryanjoshiip99999@gmail.com>
2025-02-16 00:47:23 +0530

    basic navbar designed

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../../assets/Images/logo.png";
+import cart from "../../assets/Images/shopping-cart.png";
+import shop from "../../assets/Images/shop.png";
+import user from "../../assets/Images/user.png";
+
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>
-  <h1></h1>This is Navbar<h1/>
-  </div>;
+  const [searchWidth, setSearchWidth] = useState(160);
+
+  const handleInput = (e) => {
+    const length = e.target.value.length;
+    let newWidth = 160; // Default width
+
+    if (length > 10) {
+      // Expands after 10 characters
+      newWidth = Math.min(160 + length * 5, 400); // Expands up to 400px
+    }
+
+    setSearchWidth(newWidth);
+  };
+
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="searchbar" style={{ width: `${searchWidth}px` }}>
+        <input type="text" onInput={handleInput} />
+      </div>
+
+      <div className="navbar-right">
+        <div className="cart">
+          <img src={cart} alt="cart" className="cart" />
+          <span className="cart_text">Cart</span>
+        </div>
+        <div className="seller_op">
+          <img src={shop} alt="shop" className="seller_op" />
+          <span className="seller_text">Become a Seller</span>
+        </div>
+        <div className="login">
+          <img src={user} alt="shop" className="login" />
+          <span className="login_text">Login</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
