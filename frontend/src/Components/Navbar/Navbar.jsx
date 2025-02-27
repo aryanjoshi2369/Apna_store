7aaa0f6f709271c0b70bc4183b8b152301441c5c
Aryan <aryanjoshiip99999@gmail.com>
2025-02-27 10:37:57 -0600

    Aryan: navbar made responsive

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,52 +3,49 @@ import logo from "../../assets/Images/logo.png";
 import cart from "../../assets/Images/shopping-cart.png";
 import shop from "../../assets/Images/shop.png";
 import user from "../../assets/Images/user.png";
+import { IoSearchOutline } from "react-icons/io5";
 
 import "./Navbar.css";
 
 const Navbar = () => {
-
-   
-   
-
-  const [searchWidth, setSearchWidth] = useState(160);
-
-  const handleInput = (e) => {
-    const length = e.target.value.length;
-    let newWidth = 160; // Default width
-
-    if (length > 10) {
-      // Expands after 10 characters
-      newWidth = Math.min(160 + length * 5, 400); // Expands up to 400px
-    }
-
-    setSearchWidth(newWidth);
-  };
-
   return (
     <div className="navbar">
-      <img src={logo} alt="logo" className="logo" />
-      <div className="searchbar" style={{ width: `${searchWidth}px` }}>
-        <input type="text" onInput={handleInput} />
-      </div>
+      <div className="navbar_act">
+        <img src={logo} alt="logo" className="logo" />
 
-      <div className="navbar-right">
-        <div className="cart">
-          <img src={cart} alt="cart" className="cart" />
-          <span className="cart_text">Cart</span>
+        <div className="searchbar">
+          <IoSearchOutline className="search_img" />
+          <input type="text" placeholder="Search products" />
         </div>
-        <div className="seller_op">
-          <img src={shop} alt="shop" className="seller_op" />
-          <span className="seller_text">Become a Seller</span>
+
+        <div className="navbar-right">
+          <div className="cart">
+            <img src={cart} alt="cart" className="cart" />
+            <span className="cart_text">Cart</span>
+          </div>
+
+          <div className="login">
+            <img src={user} alt="shop" className="login" />
+            <span className="login_text">Login</span>
+          </div>
+
+          <div className="seller_op">
+            <img src={shop} alt="shop" className="seller_op" />
+            <span className="seller_text">Become a Seller</span>
+          </div>
         </div>
-        <div className="login">
-          <img src={user} alt="shop" className="login" />
-          <span className="login_text">Login</span>
+      </div>
+      <div className="searchbar_mob_d">
+        <div className="searchbar_mob">
+          <IoSearchOutline className="search_img_mob" />
+          <input
+            type="text"
+            placeholder="Search products"
+            className="searchbar_mod_input"
+          />
         </div>
       </div>
     </div>
-
-
   );
 };
 
