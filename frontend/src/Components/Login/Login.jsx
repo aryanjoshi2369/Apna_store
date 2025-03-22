15dfbca250d3c93d90e14b312c822b1d8619c4b4
programmaraditya123 <officialadi4255@gmail.com>
2025-03-22 22:34:05 +0530

    login aditya frontend

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const Login = ({onClose,show}) => {
     //const [sshowlogin,ssetShowLogin] = useState(false)
+    const [isSignup, setIsSignup] = useState(true);
     const [name,setName] = useState("");
     const [email,setEmail] = useState("");
     const [pass,setPass] = useState("");
@@ -15,15 +16,25 @@ const Login = ({onClose,show}) => {
 
     const handleSubmit = async (e) => {
       e.preventDefault();
+
+      const url = isSignup 
+      ? `http://localhost:8888/api/user/register` 
+      : `http://localhost:8888/api/user/login` 
+
+      const payload = isSignup 
+      ? {name:name,email:email,password:pass}
+      : {email,password:pass}
+
        
       try {
-          const res = await axios.post(`http://localhost:8888/api/user/register`,{name:name,email:email,password:pass});
-
-          console.log("////////////////////////Account created Succesfully",res)
+          // const res = await axios.post(`http://localhost:8888/api/user/register`,{name:name,email:email,password:pass});
+             const res = await axios.post(url,payload);
+          // console.log("////////////////////////Account created Succesfully",res)
 
           
           if(res && res.data.success){
-               alert("Account Created Successfully")
+            alert(isSignup ? "Account Created Successfully" : "Login Successful");
+            onClose();
               // navigate('/login');
           } else{
               alert(res.response.data.message)
@@ -32,7 +43,7 @@ const Login = ({onClose,show}) => {
       } catch (error) {
           console.log("Error in login form",error);
           // alert("Something went wrong.Please Try Again");
-          alert(error.response.data.message)
+          alert(error.response?.data?.message || "Something Went Wrong")
           
       }
   }
@@ -40,18 +51,23 @@ const Login = ({onClose,show}) => {
     // <div className={`pagelogin ${sshowlogin? "closelogin" : ""}`}>
     <div className={`pagelogin ${show ? "" : "closelogin"}`}> 
     {/* <button className='btn-1' onClick={() =>{ssetShowLogin(true)}}><RxCross1/></button> */}
-    <button className='btn-1' onClick={onClose}><RxCross1 /></button>
+    <button className='btn-1' onClick={onClose} style={{ marginBottom: isSignup ? '26rem' : '22rem' }}><RxCross1 /></button>
     <div className='login1'>
     <form onSubmit={handleSubmit} className='loginform'>
-      <h1>Create Account</h1>
-      <h2>Already have an account?<span>Log in</span></h2>
-      <h3>Enter Your Name</h3>
+      <h1>{isSignup ? "Create Account" : "Login"}</h1>
+      <h2>{isSignup ? "Already have an account?" : "New User?"}<span onClick={() => setIsSignup(!isSignup)} style={{ cursor: "pointer", color: "blue" }}>
+      {isSignup ? "Log in" : "Sign up"}
+      </span></h2>
+      
+      {isSignup && (<> <h3>Enter Your Name</h3>
       <input type='text' value={name} onChange={(e) => setName(e.target.value)} className='loginname' placeholder='Enter Your Name' required/>
+      </>)}
+      
       <h3>Email</h3>
       <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} className='logininput' placeholder='Enter Email' required/>
       <h3>Password</h3>
       <input type='password' value={pass} onChange={(e) => setPass(e.target.value)} className='loginpass' placeholder='Enter Password' required/>
-      <button type='submit' className='signup'>Sign up</button>
+      <button type='submit' className='signup'>{isSignup ? "Sign up" : "Login"}</button>
       </form>
     </div>
     </div>
